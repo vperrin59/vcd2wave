450469c94c042bd73db253d39a667d3edb51bf1c
Victor Perrin <perrin@kandou.com>
2020-04-20 09:18:06 +0200

    Added bus conversion functions

--- vcd2wave.py
+++ vcd2wave.py
@@ -5,6 +5,7 @@ import os
 import json
 import argparse
 import re
+import math
 
 from Verilog_VCD import parse_vcd
 from Verilog_VCD import get_timescale
@@ -34,6 +35,7 @@ class Vcd2Wave(object):
         self.VAL_IDX = 1
         self.wavedrom = []
         self.clock_edge_cnt = 0
+        self.bus_format = "b"
     # def collapse_bus_sigs(self, sigs):
     #     clps_sigs = set()
     #     for s in sigs:
@@ -90,6 +92,35 @@ class Vcd2Wave(object):
         # print(self.vcd.keys())
         # print(self.vcd)
 
+    # Convert 4 state bus
+    def bus_convert(self, bus):
+        bus_w = len(bus)
+        bus_s = "".join(reversed(bus))
+        if self.bus_format == "b":
+            symb_cnt = bus_w
+            if "x" in bus:
+                return "0b" + (symb_cnt * "X")
+            elif "z" in bus:
+                return "0b" + (symb_cnt * "Z")
+            else:
+                return "0b" + bus_s
+        elif self.bus_format == "d":
+            symb_cnt = int(math.ceil(math.log10(2**bus_w)))
+            if "x" in bus:
+                return (symb_cnt * "X")
+            elif "z" in bus:
+                return (symb_cnt * "Z")
+            else:
+                return int("".join(reversed(bus)), 2)
+        elif self.bus_format == "h":
+            symb_cnt = -(bus_w // -4)
+            if "x" in bus:
+                return "0x" + (symb_cnt * "X")
+            elif "z" in bus:
+                return "0x" + (symb_cnt * "Z")
+            else:
+                return hex(int(bus_s, 2))
+
     # Collapse buses ?
     def collapse_bus_vcd(self):
         new_vcd = {}
@@ -130,7 +161,8 @@ class Vcd2Wave(object):
                         del self.vcd[k]
                         free_vcd_k = k
 
-            tv[s].append((t, "".join(bus)))
+            # tv[s].append((t, "".join(bus)))
+            tv[s].append((t, self.bus_convert(bus)))
 
             while len(bus_map[s]):
 
@@ -157,7 +189,7 @@ class Vcd2Wave(object):
 
 
                 # t = self.vcd[min_ks[0]]["tv"][signals_idx[min_ks[0]]][self.TIME_IDX]
-                tv[s].append((t, hex(int("".join(reversed(bus)), 2))))
+                tv[s].append((t, self.bus_convert(bus)))
                 # tv[s].append((t, ("".join(bus), 2)))
 
             print(tv)
@@ -204,6 +236,8 @@ class Vcd2Wave(object):
                 print(self.vcd_signal_map[k])
             else:
                 print(self.cfg["clk_name"])
+
+            self.vcd[k]["tv"] = self.win_tv[k]
             print(self.win_tv[k])
         # print(self.clock_symb)
         # print(self.vcd_signal_map)
@@ -218,7 +252,7 @@ class Vcd2Wave(object):
             self.wavedrom[k] = []
         # signals_idx = {f:0 for f in self.vcd_signal_map.keys()}
         # Loop over clock times
-        for i, (t, v) in enumerate(self.win_tv[self.clock_symb]):
+        for i, (t, v) in enumerate(self.vcd[self.clock_symb]["tv"]):
             if v == "1":
                 self.clock_edge_cnt += 1
                 # self.wavedrom.append({})
@@ -227,9 +261,9 @@ class Vcd2Wave(object):
                     # print(s_k)
                     if signals_idx[s_k] >= 0:
                         # Progress in time
-                        while self.win_tv[s_k][signals_idx[s_k]][self.TIME_IDX] < t:
+                        while self.vcd[s_k]["tv"][signals_idx[s_k]][self.TIME_IDX] < t:
                             signals_idx[s_k] += 1
-                            if signals_idx[s_k] == len(self.win_tv[s_k]):
+                            if signals_idx[s_k] == len(self.vcd[s_k]["tv"]):
                                 # Signal is constant from here
                                 signals_idx[s_k] = -1
                                 break
@@ -238,12 +272,16 @@ class Vcd2Wave(object):
                     # REVISIT: Handle the case where signal is constant
                     # print("Time clock %f signal %f" % (t, self.win_tv[s_k][signals_idx[s_k]][self.TIME_IDX]))
                     if signals_idx[s_k] < 0:
-                        self.wavedrom[s_k].append(self.win_tv[s_k][-1][self.VAL_IDX])
-                    elif t == self.win_tv[s_k][signals_idx[s_k]][self.TIME_IDX]:
-                        self.wavedrom[s_k].append(self.win_tv[s_k][signals_idx[s_k]][self.VAL_IDX])
-                    elif t < self.win_tv[s_k][signals_idx[s_k]][self.TIME_IDX]:
-                        self.wavedrom[s_k].append(self.win_tv[s_k][signals_idx[s_k] - 1][self.VAL_IDX])
+                        self.wavedrom[s_k].append(self.vcd[s_k]["tv"][-1][self.VAL_IDX])
+                    elif t == self.vcd[s_k]["tv"][signals_idx[s_k]][self.TIME_IDX]:
+                        self.wavedrom[s_k].append(self.vcd[s_k]["tv"][signals_idx[s_k]][self.VAL_IDX])
+                    elif t < self.vcd[s_k]["tv"][signals_idx[s_k]][self.TIME_IDX]:
+                        self.wavedrom[s_k].append(self.vcd[s_k]["tv"][signals_idx[s_k] - 1][self.VAL_IDX])
         print(self.wavedrom)
+
+
+    def remove_last_comma(self, s):
+        return s[:-2] + "\n"
 
     def dump_wavedrom(self):
         # print(self.vcd_signal_map)
@@ -264,23 +302,44 @@ class Vcd2Wave(object):
             indent = indent[:-2]
             f.write(indent + "},\n")
             ############## Signals
+            text = ""
             for k in self.wavedrom:
+                is_bus = int(self.vcd[k]["nets"][0]["size"]) > 1
+                bus_data = []
                 wave = ""
                 prev = ""
                 for v in self.wavedrom[k]:
                     if v == prev:
                         wave += "."
                     else:
-                        wave += v
+                        if is_bus:
+                            wave += "="
+                            bus_data.append(v)
+                        else:
+                            wave += v
                     prev = v
 
-                f.write(indent + "{\n")
+                text += indent + "{\n"
                 indent += " " * 2
-                f.write(indent + "\"name\": \"%s\",\n" % self.vcd_signal_map[k])
-                f.write(indent + "\"wave\": \"%s\"\n" % wave)
-                indent = indent[:-2]
-                f.write(indent + "},\n")
+                if is_bus:
+                    text += indent + "\"data\": [\n"
+                    indent += " " * 2
+                    for d in bus_data:
+                        text += indent + "\"%s\",\n" % d
+                        # f.write(indent + "\"%s\",\n" % d)
 
+                    text = self.remove_last_comma(text)
+
+                    text += indent + "],\n"
+                    indent = indent[:-2]
+
+                text += indent + "\"name\": \"%s\",\n" % self.vcd_signal_map[k]
+                text += indent + "\"wave\": \"%s\"\n" % wave
+                indent = indent[:-2]
+                text += indent + "},\n"
+
+            text = self.remove_last_comma(text)
+            f.write(text)
             indent = indent[:-2]
             f.write(indent + "]\n")
             indent = indent[:-2]
